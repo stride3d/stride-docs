f1a1104b25766c4e6cc9945b01a9247833540d6e
Virgile Bello <virgile@xenko.com>
2020-05-01 16:42:28 +0200

    Fix deploy script

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -7,13 +7,13 @@ Write-Host Executing `'WAWSDeploy.exe ../stride_doc.zip $deploymentProfile /v /t
 # Process versions.json
 Write-Host Updating list of versions
 $response = Invoke-RestMethod -Uri http://xenko-doc.azurewebsites.net/versions.json
-$response.versions = ($response.versions + $deploymentProfile) | select -Unique | Sort-Object -Property @{Expression={ new-object System.Version ($_) }; Descending = $True}
+$response.versions = ($response.versions + $version) | select -Unique | Sort-Object -Property @{Expression={ new-object System.Version ($_) }; Descending = $True}
 # Save file
 New-Item -Name "_siteroot" -ItemType "directory"
 $response | ConvertTo-Json | Out-File -Encoding utf8 "_siteroot/versions.json"
 
 # Only update web.config if we are the latest version
-if ($response.versions[0] -eq $deploymentProfile)
+if ($response.versions[0] -eq $version)
 {
 	(Get-Content -path "web.config" -Raw) -Replace '%deployment_version%', $version | Set-Content -Encoding utf8 -Path "_siteroot\web.config"
 }
